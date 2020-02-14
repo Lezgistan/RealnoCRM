eb004b744c49a0a1ef35f24fe3434c768c182952
Lezgistan <thehumblebimo@yandex.ru>
2020-02-14 22:49:47 +0500

    (feat) add migration for creating doc_version_table

---
+++ database/migrations/2020_02_14_101913_create_doc_version_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocVersionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doc_version', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('document_id');
+            $table->foreign('document_id')->references('id')->on('user_docs')->onDelete('cascade');
+            $table->unsignedInteger('version')->default(1);
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unique(['document_id','version']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doc_version');
+    }
+}
