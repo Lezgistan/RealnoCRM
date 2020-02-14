151e9665d1514cd08b1fa02377880b05711e5cb8
Lezgistan <thehumblebimo@yandex.ru>
2020-02-14 23:00:12 +0500

    (feat) add create.blade for documents

---
+++ resources/views/users/documents/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container d-flex justify-content-center w-100">
+        <div class="w-25">
+            {{ Form::open(['url'=>route('documents.store'),'files'=>true,'method'=>'POST',]) }}
+            @include('form._input',[
+                'name'=>'name',
+                'required'=>true,
+                'label'=>'Название документа'
+            ])
+            @include('form._file',[
+          'name'=>'image',
+           'type'=>'file',
+           'required'=>true,
+           'label'=>'Выбрать файл',
+      ])
+            <button class="btn btn-primary w-100 mt-3">Создать</button>
+            {{ Form::close() }}
+        </div>
+    </div>
+@stop
