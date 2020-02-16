6782beb463ed3ba64ff111162bd04a94aa2453f7
Lezgistan <thehumblebimo@yandex.ru>
2020-02-16 23:55:35 +0500

    (feat) add getters and setters for id of document

--- app/Models/Users/UserDoc.php
+++ app/Models/Users/UserDoc.php
@@ -110,7 +110,22 @@ class UserDoc extends Model
         $this->user_id = $user_id;
     }
 
-  
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
     /**
      * @return BelongsTo
      */
