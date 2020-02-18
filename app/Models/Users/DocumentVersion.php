a624e82514317b0c24d04d717f7c03aed7db6f18
Lezgistan <thehumblebimo@yandex.ru>
2020-02-18 19:23:06 +0500

    (feat) add getters and setters to DocumentVersion Model

--- app/Models/Users/DocumentVersion.php
+++ app/Models/Users/DocumentVersion.php
@@ -36,11 +36,75 @@ class DocumentVersion extends Model
     ];
 
     /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDocumentId(): int
+    {
+        return $this->document_id;
+    }
+
+    /**
+     * @param int $document_id
+     */
+    public function setDocumentId(int $document_id): void
+    {
+        $this->document_id = $document_id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVersion(): int
+    {
+        return $this->version;
+    }
+
+    /**
+     * @param int $version
+     */
+    public function setVersion(int $version): void
+    {
+        $this->version = $version;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param int|null $user_id
+     */
+    public function setUserId(?int $user_id): void
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
      * @return BelongsTo
      */
     public function document(): belongsTo
     {
-        return $this->belongsTo(UserDoc::class);
+        return $this->belongsTo(UserDoc::class,'id','document_id');
     }
 
     /**
@@ -50,4 +114,5 @@ class DocumentVersion extends Model
     {
         return $this->belongsTo(User::class);
     }
+
 }
